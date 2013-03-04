fe231adf3a64c81fa9629c389281b0eaeca7d2fc
grosser <grosser.michael@gmail.com>
2013-03-04 13:58:11 -0800

    wrong user -> fails to start

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,5 +3,5 @@ default["memcached"]["listen"]          = "127.0.0.1"
 default["memcached"]["log_file"]        = "/var/log/memcached.log"
 default["memcached"]["max_memory"]      = 64
 default["memcached"]["max_connections"] = 1024
-default["memcached"]["user"]            = "memcached"
+default["memcached"]["user"]            = "memcache"
 default["memcached"]["verbose"]         = false
